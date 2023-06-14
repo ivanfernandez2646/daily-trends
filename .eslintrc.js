59bb0f51cdf6b2f8aff7865aa53692d2c442fa93
Ivan Fernandez OM <ivan.fernandezcampos@optivamedia.com>
2023-06-14 12:06:03 +0200

    style: eslintrc more flexible

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,22 @@
 module.exports = {
-	extends: ['eslint-config-codely/typescript'],
-	rules: {
-		'no-console': 'warn'
-	},
-	overrides: [
-		{
-			files: ['*.ts', '*.tsx'],
-			parserOptions: {
-				project: ['./tsconfig.json']
-			},
-			rules: {
-				'@typescript-eslint/no-floating-promises': 'warn'
-			}
-		}
-	]
+  extends: ['eslint-config-codely/typescript'],
+  rules: {
+    'no-console': 'warn'
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: ['./tsconfig.json']
+      },
+      rules: {
+        '@typescript-eslint/no-floating-promises': 'warn',
+        '@typescript-eslint/no-unsafe-member-access': 'warn',
+        '@typescript-eslint/no-unsafe-call': 'warn',
+        '@typescript-eslint/no-unsafe-assignment': 'warn',
+        '@typescript-eslint/no-unsafe-argument': 'warn',
+        '@typescript-eslint/no-unsafe-return': 'warn'
+      }
+    }
+  ]
 };
