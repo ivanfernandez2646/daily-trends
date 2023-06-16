0180559b59715e2ddb3866c373820004e554eaa0
Ivan Fernandez OM <ivan.fernandezcampos@optivamedia.com>
2023-06-16 09:42:07 +0200

    fix(@contexts/cms): uuid value object exception naming

--- src/contexts/cms/shared/domain/UuidValueObject.ts
+++ src/contexts/cms/shared/domain/UuidValueObject.ts
@@ -4,23 +4,23 @@ import InvalidArgumentError from './InvalidArgumentError';
 import RequiredStringValueObject from './RequiredStringValueObject';
 
 export default class UuidValueObject extends RequiredStringValueObject {
-	constructor(value: string) {
-		UuidValueObject.ensureIsValidUuid(value);
+  constructor(value: string) {
+    UuidValueObject.ensureIsValidUuid(value, new.target.name);
 
-		super(value);
-	}
+    super(value);
+  }
 
-	static random(): UuidValueObject {
-		return new UuidValueObject(v4());
-	}
+  static random(): UuidValueObject {
+    return new UuidValueObject(v4());
+  }
 
-	private static ensureIsValidUuid(id: string): void {
-		if (!validate(id)) {
-			throw new InvalidArgumentError(`<${this.constructor.name}> does not allow the value <${id}>`);
-		}
-	}
+  private static ensureIsValidUuid(id: string, inheritedInstanceClassName: string): void {
+    if (!validate(id)) {
+      throw new InvalidArgumentError(`<${inheritedInstanceClassName}> does not allow the value <${id}>`);
+    }
+  }
 
-	equalsTo(other: UuidValueObject): boolean {
-		return this.value === other.value;
-	}
+  equalsTo(other: UuidValueObject): boolean {
+    return this.value === other.value;
+  }
 }
